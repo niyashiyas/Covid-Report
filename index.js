fdbe4dc17f31bc15ed96818f54a0863aeb32bc2c
Niya Shiyas <niyashiyas03@gmail.com>
2022-11-03 21:50:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function updateMap(){
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+
+        rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+            cases = element.infected;
+            if (cases>300){
+                color = "rgb(255, 0, 0)";
+                // innerHeight = 100px;
+            }else{
+                color = `rgb(${cases}, 0, 0)`;
+            }
+
+        //To Mark on the map
+        new mapboxgl.Marker({
+            draggable: false,
+            color: color
+        }).setLngLat([longitude, latitude])
+        .addTo(map); 
+    });
+})
+}
+
+let interval = 4000;
+setInterval( updateMap, interval);  
