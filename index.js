4aeb1b9d39d1c95fa51f2eed9eb2e4a542b9fd52
Niya Shiyas <niyashiyas03@gmail.com>
2023-01-14 23:09:53 +0530

    changed file

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function updateMap() {
-  console.log("Updating map with realtime data");
+  console.log("Updating map with data from 27/03/20");
   fetch("/data.json")
     .then((response) => response.json())
     .then((rsp) => {
-      // console.log(rsp.data)
+      console.log(rsp.data);
       rsp.data.forEach((element) => {
         latitude = element.latitude;
         longitude = element.longitude;
 
         cases = element.infected;
         if (cases > 255) {
-          color = "rgb(255, 0, 0)";
+          color = "rgb(220, 40, 80)";
+        } else {
+          color = `rgb(${cases}, 0, 0)`;
         }
 
         //To Mark on the map
-        const marker = new mapboxgl.Marker()
-          //     {
-          //     draggable: false,
-          //     color: color
-          // }
+        const marker = new mapboxgl.Marker({
+          draggable: false,
+          color: color,
+        })
           .setLngLat([longitude, latitude])
           .addTo(map);
       });
